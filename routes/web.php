adc731d9eba21b2c387a3b35e33f1ce3d7264dd9
Lulu123hh <lulu123@gmail.com>
2025-09-21 13:51:08 -0600

    modificacion

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UsuarioController;
 
+// Cuando entren a "/", ejecuta directamente el método index del controlador
+Route::get('/', [UsuarioController::class, 'index']);
+
+// CRUD de usuarios
 Route::resource('usuarios', UsuarioController::class);
